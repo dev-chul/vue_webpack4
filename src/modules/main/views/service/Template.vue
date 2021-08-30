42e0c2f5fefbfc765dd3d5c062d10d1cef2d24a7
lostjc001 <lostjc001@gmail.com>
2021-08-30 14:18:47 +0900

    Adding Template Sample Page

---
+++ src/modules/main/views/service/Template.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Template',
+  data () {
+    return {
+      msg: 'Template Sample Page'
+    }
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
