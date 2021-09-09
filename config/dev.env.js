7731fa9f70b7e756deda28d52ad28db6d66b9c37
lostjc001 <lostjc001@gmail.com>
2021-09-09 23:49:44 +0900

    modifying configuration.

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // VUE_APP_BASE_API: '"http://158.247.220.52:9100"'
-  VUE_APP_BASE_API: '"http://127.0.0.1:8090"'
+  VUE_APP_BASE_API: '"http://146.56.174.154:8100"'
 })
